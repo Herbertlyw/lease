e7b9fb7d43d2e65a775949fba8bcf2b70c90c162
Herbert <2142354019@qq.com>
2025-10-16 22:39:49 +0800

    10.16

---
+++ common/src/main/java/com/atguigu/lease/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.atguigu.lease.common.exception;
+
+import com.atguigu.lease.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public Result handleException(Exception e) {
+        e.printStackTrace();
+        return Result.fail();
+    }
+
+}
---
+++ common/src/main/java/com/atguigu/lease/common/minio/MinioProperties.java
@@ -0,0 +1,20 @@
+package com.atguigu.lease.common.minio;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@ConfigurationProperties(prefix = "minio")
+public class MinioProperties {
+
+    private String endpoint;
+
+    private String accessKey;
+
+    private String secretKey;
+
+    private String bucketName;
+}
